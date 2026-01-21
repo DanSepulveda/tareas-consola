a93920d750fc92287f00cbccf9482378ecaa996b
Daniel Sepúlveda <dansepdev@gmail.com>
2026-01-21 18:13:34 -0300

    data files paths added

--- src/constantes.py
+++ src/constantes.py
@@ -1,7 +1,15 @@
 from enum import StrEnum
 
 
-# MENSAJES DE ÉXITO/ERROR PARA DAR FEEDBACK A LOS USUARIOS AL REALIZAR CIERTAS ACCIONES
+# Rutas de los distintos archivos (datos), y de las carpetas "reportes" y "logs"
+class Rutas(StrEnum):
+    USUARIOS = "datos/usuarios.json"
+    TAREAS = "datos/tareas.xlsx"
+    REPORTES = "reportes/"
+    LOGS = "logs/"
+
+
+# Mensajes de éxito/error para dar feedback a los usuarios dependiendo de las acciones
 class Mensajes(StrEnum):
     INVALID_INT = "Opción inválida. Posibles valores:"
     NO_INT = "Debe ingresar un número entero."
