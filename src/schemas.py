e0d50416105cb06ae4cb574314e07591e25757de
Daniel Sepúlveda <dansepdev@gmail.com>
2026-01-22 00:16:30 -0300

    type Usuario added

--- src/schemas.py
+++ src/schemas.py
@@ -1,6 +1,15 @@
 from typing import TypedDict
 
 
+# Type para menús que despliegan opciones
 class Menu(TypedDict):
     titulo: str
     opciones: list[str]
+
+
+# Type para usuarios almacenados en usuarios.json
+class Usuario(TypedDict):
+    id: str
+    nombre: str
+    nombre_usuario: str
+    clave: str
