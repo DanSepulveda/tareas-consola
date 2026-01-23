e23c7ef9737b8791982823127d7a4740dc88f15b
Daniel Sepúlveda <dansepdev@gmail.com>
2026-01-23 16:34:05 -0300

    app name added and docstring removed

--- src/constantes.py
+++ src/constantes.py
@@ -1,17 +1,19 @@
 """
 Configuración de constantes globales del sistema.
 
-Define rutas de archivos y enumeraciones estáticas utilizadas
-para mantener la consistencia en toda la aplicación.
+Define enumeraciones estáticas utilizadas para mantener la
+consistencia en toda la aplicación (rutas, configuraciones, etc).
 """
 
 from enum import StrEnum
 
 
 class Rutas(StrEnum):
-    """Rutas de los archivos (datos), y de las carpetas 'reportes' y 'logs'"""
-
     USUARIOS = "datos/usuarios.csv"
     TAREAS = "datos/tareas.json"
     REPORTES = "reportes/"
     LOGS = "logs/"
+
+
+class Config(StrEnum):
+    NOMBRE_APP = "tareApp"
