abbd759ca16675882c4e816a591e09dc720c7129
Daniel Sepúlveda <dansepdev@gmail.com>
2026-01-23 09:03:25 -0300

    add: Tarea and Estado Schemas and docstring

--- src/schemas.py
+++ src/schemas.py
@@ -1,15 +1,43 @@
-from typing import TypedDict
+"""
+Definición de estructuras de datos y tipos (Schemas).
+
+Este módulo define la forma de los objetos de datos para
+asegurar la consistencia y facilitar el autocompletado.
+"""
+
+from typing import Literal, TypedDict
 
 
-# Type para menús que despliegan opciones
 class Menu(TypedDict):
+    """Type para los menús de navegación."""
+
     titulo: str
     opciones: list[str]
 
 
-# Type para usuarios almacenados en usuarios.json
 class Usuario(TypedDict):
+    """Type para usuarios almacenados en usuarios.csv"""
+
     id: str
     nombre: str
     nombre_usuario: str
     clave: str
+
+
+class Tarea(TypedDict):
+    """Type para tareas registradas en tareas.json"""
+
+    id: str
+    id_usuario: str
+    fecha_creacion: str
+    fecha_vencimiento: str | None
+    titulo: str
+    categoria: str
+    estado: Literal["Pendiente", "En proceso", "Finalizada"]
+
+
+class Estado(TypedDict):
+    """Type para el estado de la App (usuario logueado y sus tareas)"""
+
+    usuario: Usuario
+    tareas: list[Tarea]
