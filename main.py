570f690b0f104bd0e157768b32b9822e5edca683
Daniel Sepúlveda <dansepdev@gmail.com>
2026-01-23 17:19:50 -0300

    add: feedback for users in console

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import src.repositorio as repo
 import src.utilidades.consola as cli
 from src.acceso import login
+from src.constantes import Config
 from src.navegacion import menu_principal
 
 
 def main():
-    usuario_logueado = login()
+    cli.print_panel(
+        titulo=Config.NOMBRE_APP,
+        contenido=f"Bienvenido a [green_yellow]{Config.NOMBRE_APP}[/], el programa N°1 para la Administración de Tareas.",
+        subtitulo="Inicie sesión para continuar",
+    )
 
+    usuario_logueado = login()
     if not usuario_logueado:
-        cli.print_error("No se ha logueado ningún usuario.")
+        cli.print_alerta("No se ha iniciado sesión con ningún usuario.")
         return
 
     tareas = repo.obtener_tareas_usuario(usuario_logueado["id"])
-    menu_principal(estado={"usuario": usuario_logueado, "tareas": tareas})
+    cli.input_continuar("¡Acceso concedido!")
+    menu_principal({"usuario": usuario_logueado, "tareas": tareas})
 
 
 if __name__ == "__main__":
     try:
         main()
     except Exception as e:
-        print(e)
+        cli.print_error(str(e))
+    finally:
+        cli.print_panel(
+            titulo=Config.NOMBRE_APP,
+            contenido="App finalizada. ¡Hasta pronto! 🤝",
+            limpiar=False,
+        )
