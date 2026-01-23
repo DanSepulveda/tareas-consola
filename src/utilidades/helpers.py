9bba5c489145cd949b79c0fb1732752d423a8dcf
Daniel Sepúlveda <dansepdev@gmail.com>
2026-01-23 16:17:53 -0300

    refector: function name changed

--- src/utilidades/helpers.py
+++ src/utilidades/helpers.py
@@ -11,35 +11,16 @@ from pathlib import Path
 
 
 def crear_directorio(ruta: str) -> None:
-    """
-    Crea las carpetas de una ruta en caso de no existir.
-
-    Args:
-        ruta (str): Ruta a crear.
-    """
+    """Crea las carpetas de la ruta indicada."""
     path = Path(ruta).parent
     path.mkdir(parents=True, exist_ok=True)
 
 
+def generar_hash(clave: str) -> str:
+    """Genera un hash para la clave del usuario."""
+    return hashlib.sha256(clave.encode("utf-8")).hexdigest()
+
+
 def generar_id() -> str:
-    """
-    Genera un identificador único.
-
-    Returns:
-        str: Identificador único (id).
-    """
+    """Genera un identificador único."""
     return str(uuid.uuid4())
-
-
-def hash_clave(clave: str) -> str:
-    """
-    Obtiene el hash para la clave del usuario.
-
-    Args:
-        clave (str): Clave del usuario.
-
-    Returns:
-        str: Hash de la clave.
-    """
-    hash_objeto = hashlib.sha256(clave.encode("utf-8"))
-    return hash_objeto.hexdigest()
